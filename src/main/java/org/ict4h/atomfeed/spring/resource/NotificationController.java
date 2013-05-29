aff599de430f24d9f4b5e195337f79d3b9c3f561
Pulkit <pulkit.bosco05@gmail.com>
2013-05-30 00:51:07 +0530

    Adding ability to communicate with client via controllers.

--- src/main/java/org/ict4h/atomfeed/spring/resource/NotificationController.java
+++ src/main/java/org/ict4h/atomfeed/spring/resource/NotificationController.java
@@ -1,29 +1,38 @@
 package org.ict4h.atomfeed.spring.resource;
 
+import org.ict4h.atomfeed.client.service.AtomFeedClient;
+import org.ict4h.atomfeed.client.service.EventWorker;
 import org.ict4h.atomfeed.server.service.Event;
 import org.ict4h.atomfeed.server.service.EventFeedService;
 import org.ict4h.atomfeed.server.service.EventService;
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.UUID;
 
 @Controller
 public class NotificationController {
 
-    EventFeedService eventFeedService;
-    private final EventService eventService;
+    private static Logger logger = LoggerFactory.getLogger(NotificationController.class);
+
+    private EventService eventService;
+    private AtomFeedClient atomFeedClient;
 
     @Autowired
-    public NotificationController(EventFeedService eventFeedService, EventService eventService) {
-        this.eventFeedService = eventFeedService;
+    public NotificationController(EventService eventService, AtomFeedClient atomFeedClient) {
         this.eventService = eventService;
+        this.atomFeedClient = atomFeedClient;
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/entry/new")
@@ -43,5 +52,42 @@ public class NotificationController {
                 "standalone");
         eventService.notify(event);
         return showForm(new ModelMap());
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/processevents")
+    @ResponseBody
+    public String processEvents(String feedUri, String processType) throws URISyntaxException {
+        EventWorker eventWorker = (processType.equalsIgnoreCase("fail"))
+                ? new FailEventWorker() : new SuccessEventWorker();
+
+        atomFeedClient.processEvents(new URI(feedUri), eventWorker);
+
+        return "Finished processing events.\n";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/processfailedevents")
+    @ResponseBody
+    public String processFailedEvents(String feedUri, String processType) throws URISyntaxException {
+        EventWorker eventWorker = (processType.equalsIgnoreCase("fail"))
+                ? new FailEventWorker() : new SuccessEventWorker();
+
+        atomFeedClient.processFailedEvents(new URI(feedUri), eventWorker);
+
+        return "Finished processing failed events.\n";
+    }
+
+    class FailEventWorker implements EventWorker {
+        @Override
+        public void process(org.ict4h.atomfeed.client.domain.Event event) {
+            logger.info("Failing Event" + event);
+            throw new RuntimeException();
+        }
+    }
+
+    class SuccessEventWorker implements EventWorker {
+        @Override
+        public void process(org.ict4h.atomfeed.client.domain.Event event) {
+            logger.info("Successfully processing " + event);
+        }
     }
 }
