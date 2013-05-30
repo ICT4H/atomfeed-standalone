36260b008268a8c39d95a551794b13be3f67be2d
Pulkit <pulkit.bosco05@gmail.com>
2013-05-30 12:13:06 +0530

    Adding transactions to controller.

--- src/main/java/org/ict4h/atomfeed/spring/resource/NotificationController.java
+++ src/main/java/org/ict4h/atomfeed/spring/resource/NotificationController.java
@@ -1,20 +1,19 @@
 package org.ict4h.atomfeed.spring.resource;
 
-import org.ict4h.atomfeed.client.service.AtomFeedClient;
 import org.ict4h.atomfeed.client.service.EventWorker;
+import org.ict4h.atomfeed.client.service.FeedClient;
 import org.ict4h.atomfeed.server.service.Event;
-import org.ict4h.atomfeed.server.service.EventFeedService;
 import org.ict4h.atomfeed.server.service.EventService;
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.net.URI;
@@ -27,21 +26,25 @@ public class NotificationController {
     private static Logger logger = LoggerFactory.getLogger(NotificationController.class);
 
     private EventService eventService;
-    private AtomFeedClient atomFeedClient;
+    private FeedClient feedClient;
+
+    public NotificationController() {}
 
     @Autowired
-    public NotificationController(EventService eventService, AtomFeedClient atomFeedClient) {
+    public NotificationController(EventService eventService, FeedClient feedClient) {
         this.eventService = eventService;
-        this.atomFeedClient = atomFeedClient;
+        this.feedClient = feedClient;
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/entry/new")
+    @Transactional
     public String showForm(ModelMap model) {
         model.addAttribute("entry", new Entry());
         return "form";
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/entry/new")
+    @Transactional
     public String addEntry(Entry entry, BindingResult result) throws URISyntaxException {
         Event event = new Event(
                 UUID.randomUUID().toString(),
@@ -56,22 +59,24 @@ public class NotificationController {
 
     @RequestMapping(method = RequestMethod.POST, value = "/processevents")
     @ResponseBody
+    @Transactional
     public String processEvents(String feedUri, String processType) throws URISyntaxException {
         EventWorker eventWorker = (processType.equalsIgnoreCase("fail"))
                 ? new FailEventWorker() : new SuccessEventWorker();
 
-        atomFeedClient.processEvents(new URI(feedUri), eventWorker);
+        feedClient.processEvents(new URI(feedUri), eventWorker);
 
         return "Finished processing events.\n";
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/processfailedevents")
     @ResponseBody
+    @Transactional
     public String processFailedEvents(String feedUri, String processType) throws URISyntaxException {
         EventWorker eventWorker = (processType.equalsIgnoreCase("fail"))
                 ? new FailEventWorker() : new SuccessEventWorker();
 
-        atomFeedClient.processFailedEvents(new URI(feedUri), eventWorker);
+        feedClient.processFailedEvents(new URI(feedUri), eventWorker);
 
         return "Finished processing failed events.\n";
     }
